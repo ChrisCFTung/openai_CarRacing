050cb6534ef5d39d2ab922ec16104eeb2db9ac22
Chris Tung <christung616@gmail.com>
2022-02-10 12:55:57 -0500

    added args for running the model

--- run_model.py
+++ run_model.py
@@ -16,31 +16,38 @@ from gym.wrappers import FrameStack, Monitor
 from gym_torch import *
 from agent import *
 
-import os    
+import os, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir', dest='chkdir', type=str, action='store',
+                    help='directory name of the checkpoint')
+parser.add_argument('--id', dest='chkid', type=str, action='store',
+                    help='id of the checkpoint version')
+parser.add_argument('--eps', dest='epsilon', type=float, action='store',
+                    default = 0.1,
+                    help='value of the epsilon for the agent')
+args = parser.parse_args()
+
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 env = env_make(skip=2, stack=3)
 #env = Monitor(env, './video', force=True)
 state = env.reset()
 lulu = lewis(state_dim=env.observation_space.shape, action_dim=env.action_space.high[0]+1, save_dir=None)
-print("Please input the folder name in checkpoints")
-folder = input()
-lulu.load(f'checkpoints\\{folder}\\lewis_net_9.chkpt')
-print("Please input the desired epsilon")
-#eps = input()
-#if float(eps) >= 0.:
-#    lulu.exploration_rate=float(eps)
+folder = args.chkdir
+idx = args.chkid
+lulu.load(f'checkpoints\\{folder}\\lewis_net_{idx}.chkpt')
+eps = args.epsilon
+if float(eps) >= 0.:
+   lulu.exploration_rate=float(eps)
 print(lulu.exploration_rate)
 
 
-while True:
+for frame in range(2000):
     env.render()
     action = lulu.act(state)
     next_state, reward, done, info = env.step(action)
     state = next_state
-    
-#     done = False
-#     if done:
-#         print(done, info)
-#         break
-        
+    # if done:
+    #     print(done, info)
+    #     break
 env.close()
